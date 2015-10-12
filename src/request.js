5da5e1298c7c566ba9fff4920adb3e51f728f41b
jhusain <jhusain@gmail.com>
2015-10-12 14:55:06 -0700

    Initial Commit

---
+++ src/request.js
@@ -0,0 +1,86 @@
+'use strict';
+var getXMLHttpRequest = require('./getXMLHttpRequest');
+var getCORSRequest = require('./getCORSRequest');
+var hasOwnProp = Object.prototype.hasOwnProperty;
+var restify = require('restify');
+
+var noop = function() {};
+
+function Observable() {}
+
+Observable.create = function(subscribe) {
+  var o = new Observable();
+
+  o.subscribe = function(onNext, onError, onCompleted) {
+
+    var observer;
+    var disposable;
+
+    if (typeof onNext === 'function') {
+        observer = {
+            onNext: onNext,
+            onError: (onError || noop),
+            onCompleted: (onCompleted || noop)
+        };
+    } else {
+        observer = onNext;
+    }
+
+    disposable = subscribe(observer);
+
+    if (typeof disposable === 'function') {
+      return {
+        dispose: disposable
+      };
+    } else {
+      return disposable;
+    }
+  };
+
+  return o;
+};
+
+function request(method, options, context) {
+  return Observable.create(function requestObserver(observer) {
+
+    var config = {
+      method: method || 'GET',
+      headers: {}
+    };
+
+    var isDone,
+      prop,
+      client;
+
+    for (prop in options) {
+      if (hasOwnProp.call(options, prop)) {
+        config[prop] = options[prop];
+      }
+    }
+
+    client = restify.createJSONClient(config);
+  
+    // allow the user to mutate the config open
+    if (context.onBeforeRequest != null) {
+      context.onBeforeRequest(config);
+    }
+
+    client[config.method]({ version: '*' }, function(error, req, res, obj) {
+      if (!isDone) {
+        if (error) {
+          observer.onError(error);
+        }
+        else {
+          observer.onNext(obj);
+          observer.onCompleted();
+        }
+      }
+    });
+
+    return function dispose() {
+        isDone = true;
+    };//Dispose
+  });
+}
+
+module.exports = request;
